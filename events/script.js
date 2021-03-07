c06f051b087cce4425c8e5dc07600b06a9dd87d9
Keval Sanghvi <kevalssanghvi100@gmail.com>
2021-03-08 00:42:57 +0530

    Changes in Events Page

--- events/script.js
+++ events/script.js
@@ -2,14 +2,17 @@ var keys = [
     "Verve",
     "Battle Of Bands",
     "Symphony Idol",
-    "Natsamrat",
     "Decibels",
+    "Natsamrat",
     "Ok Doodle",
     "Picture This",
     "Feel The Beat",
     "Film Flick",
-    "Word Smith"
+    "Word Smith",
+    "Somaiya Got Talent",
+    "Mr. & Ms. Somaiya"
 ];
+
 var slider = new Swiper(".swiper-container", {
     // Optional parameters
     slidesPerView: "auto",
@@ -48,7 +51,7 @@ slider.on("slideChange", function () {
 
 slider.on("slideChangeTransitionEnd", function () {
     gsap.to(".swiper-slide .slide-img", 1, {
-        rotation: 10
+        rotation: 5
     });
     gsap.to(".slide-text span", 0.2, {
         x: 0,
@@ -134,3 +137,83 @@ gsap.to(".swiper-slide-active", 0, {
 //    $(".outer").removeClass('hidden');
 //    $(".loader-gif").removeClass(".loader-gif");
 //}, 4000);
+
+$(function () {
+    $("#sectiontwo").css("background-color", "#07334e");
+    window.addEventListener("wheel", function () {
+        if ($(".swiper-slide-active").hasClass("s-1")) {
+            $("#sectiontwo").css("background-color", "#07334e");
+        }
+        if ($(".swiper-slide-active").hasClass("s-2")) {
+            $("#sectiontwo").css("background-color", "#2e3637");
+        }
+        if ($(".swiper-slide-active").hasClass("s-3")) {
+            $("#sectiontwo").css("background-color", "#471f04");
+        }
+        if ($(".swiper-slide-active").hasClass("s-4")) {
+            $("#sectiontwo").css("background-color", "#450018");
+        }
+        if ($(".swiper-slide-active").hasClass("s-5")) {
+            $("#sectiontwo").css("background-color", "#47200a");
+        }
+        if ($(".swiper-slide-active").hasClass("s-6")) {
+            $("#sectiontwo").css("background-color", "#29425a");
+        }
+        if ($(".swiper-slide-active").hasClass("s-7")) {
+            $("#sectiontwo").css("background-color", "#4a0021");
+        }
+        if ($(".swiper-slide-active").hasClass("s-8")) {
+            $("#sectiontwo").css("background-color", "#28012e");
+        }
+        if ($(".swiper-slide-active").hasClass("s-9")) {
+            $("#sectiontwo").css("background-color", "#212a07");
+        }
+        if ($(".swiper-slide-active").hasClass("s-10")) {
+            $("#sectiontwo").css("background-color", "#32210e");
+        }
+        if ($(".swiper-slide-active").hasClass("s-11")) {
+            $("#sectiontwo").css("background-color", "#08424e");
+        }
+        if ($(".swiper-slide-active").hasClass("s-12")) {
+            $("#sectiontwo").css("background-color", "#650137");
+        }
+    });
+    $(".planet-links").on("click", "div", function () {
+        if($(".swiper-pagination-bullet-active").text() == "Verve") {
+            $("#sectiontwo").css("background-color", "#07334e");
+        }
+        if($(".swiper-pagination-bullet-active").text() == "Battle Of Bands") {
+            $("#sectiontwo").css("background-color", "#2e3637");
+        }
+        if($(".swiper-pagination-bullet-active").text() == "Symphony Idol") {
+            $("#sectiontwo").css("background-color", "#471f04");
+        }
+        if($(".swiper-pagination-bullet-active").text() == "Decibels") {
+            $("#sectiontwo").css("background-color", "#450018");
+        }
+        if($(".swiper-pagination-bullet-active").text() == "Natsamrat") {
+            $("#sectiontwo").css("background-color", "#47200a");
+        }
+        if($(".swiper-pagination-bullet-active").text() == "Ok Doodle") {
+            $("#sectiontwo").css("background-color", "#29425a");
+        }
+        if($(".swiper-pagination-bullet-active").text() == "Picture This") {
+            $("#sectiontwo").css("background-color", "#4a0021");
+        }
+        if($(".swiper-pagination-bullet-active").text() == "Feel The Beat") {
+            $("#sectiontwo").css("background-color", "#28012e");
+        }
+        if($(".swiper-pagination-bullet-active").text() == "Film Flick") {
+            $("#sectiontwo").css("background-color", "#212a07");
+        }
+        if($(".swiper-pagination-bullet-active").text() == "Word Smith") {
+            $("#sectiontwo").css("background-color", "#32210e");
+        }
+        if($(".swiper-pagination-bullet-active").text() == "Somaiya Got Talent") {
+            $("#sectiontwo").css("background-color", "#08424e");
+        }
+        if($(".swiper-pagination-bullet-active").text() == "Mr. & Ms. Somaiya") {
+            $("#sectiontwo").css("background-color", "#650137");
+        }
+    });
+});
