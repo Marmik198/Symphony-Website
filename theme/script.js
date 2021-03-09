4853b4167ff891f5c8c3884e1b7104e6086de30d
Keval Sanghvi <kevalssanghvi100@gmail.com>
2021-03-09 16:27:10 +0530

    navbar changes and theme changes

--- theme/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("#carousel-wrapper").owlCarousel({
-        autoplay: true,
-        autoplaySpeed: 100,
-        dots: true,
-        items: 1,
-        rewind: true,
-    });
-});
