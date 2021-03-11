28aa67cd8c152bc4517bd42682c48d6b495c6c31
Keval Sanghvi <kevalssanghvi100@gmail.com>
2021-03-11 18:38:12 +0530

    about page layout change

--- aboutus/script.js
+++ aboutus/script.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
     $("#carousel-wrapper").owlCarousel({
         autoplay: true,
         autoplaySpeed: 100,
-        dots: true,
+        nav: true,
+        navText: ['<i class="lni lni-chevron-left-circle"></i>', '<i class="lni lni-chevron-right-circle"></i>'],
+        dots: false,
         items: 1,
         rewind: true,
     });
